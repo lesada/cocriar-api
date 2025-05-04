8d635b8a28344232cce8f0212aaa7a4a22422a66
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-05-04 13:00:14 -0300

    test: setup mock server

--- src/test/routes/health.test.ts
+++ src/test/routes/health.test.ts
@@ -1,18 +1,9 @@
-import { healthRoutes } from "@/http/routes/health";
-import { fastify } from "fastify";
-import supertest from "supertest";
-import { beforeEach, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
+import { mockServer } from "../../../vitest.setup";
 
 describe("http > routes > health", () => {
-	const app = fastify();
-
-	beforeEach(async () => {
-		app.register(healthRoutes, { prefix: "/health" });
-		await app.ready();
-	});
-
 	it("should return 200 and status ok", async () => {
-		const response = await supertest(app.server).get("/health").expect(200);
+		const response = await mockServer.get("/health").expect(200);
 		expect(response.body).toEqual({ status: "ok" });
 	});
 });
