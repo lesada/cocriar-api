e3e0dd3917473629092bd3016e1de6ddf6ccd9ef
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-06-13 16:43:27 -0300

    test: update article

--- src/tests/controllers/articles/update-article.test.ts
+++ src/tests/controllers/articles/update-article.test.ts
@@ -1,60 +1,19 @@
-import type { FastifyReply, FastifyRequest } from "fastify";
-import { describe, expect, test, vi } from "vitest";
+import { app } from "@/app";
+import supertest from "supertest";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
 
 describe("controllers > update-article", () => {
-	const mockArticleId = "9ad64f26-9ef7-4e32-a6c7-72e261cf8c3d";
-	const mockUpdateData = {
-		title: "Updated title",
-		description: "Updated desc",
-	};
-
-	test("should return 400 if params are invalid", async () => {
-		const req = {
-			params: { article_id: "invalid-uuid" },
-			body: mockUpdateData,
-		} as unknown as FastifyRequest;
-
-		const send = vi.fn();
-		const status = vi.fn(() => ({ send }));
-		const rep = { status } as unknown as FastifyReply;
-
-		const { updateArticle } = await import(
-			"@/http/controllers/articles/update-article"
-		);
-
-		await updateArticle(req, rep);
-
-		expect(status).toHaveBeenCalledWith(400);
-		expect(send).toHaveBeenCalledWith(
-			expect.objectContaining({
-				error: "Invalid params",
-				issues: expect.any(Object),
-			}),
-		);
+	beforeAll(async () => {
+		await app.ready();
 	});
 
-	test("should return 400 if body is invalid", async () => {
-		const req = {
-			params: { article_id: mockArticleId },
-			body: { title: 123 }, // título inválido
-		} as unknown as FastifyRequest;
+	afterAll(async () => {
+		await app.close();
+	});
 
-		const send = vi.fn();
-		const status = vi.fn(() => ({ send }));
-		const rep = { status } as unknown as FastifyReply;
+	test("should return 400 if params are invalid", async () => {
+		const response = await supertest(app.server).patch("/articles/1");
 
-		const { updateArticle } = await import(
-			"@/http/controllers/articles/update-article"
-		);
-
-		await updateArticle(req, rep);
-
-		expect(status).toHaveBeenCalledWith(400);
-		expect(send).toHaveBeenCalledWith(
-			expect.objectContaining({
-				error: "Invalid body schema",
-				issues: expect.any(Object),
-			}),
-		);
+		expect(response.statusCode).toBe(400);
 	});
 });
