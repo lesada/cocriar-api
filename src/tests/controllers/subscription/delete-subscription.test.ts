0a64de2803add53a76050c12ad623bcb925bd958
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-06-16 16:30:31 -0300

    test: fix subscription test

--- src/tests/controllers/subscription/delete-subscription.test.ts
+++ src/tests/controllers/subscription/delete-subscription.test.ts
@@ -1,111 +1,52 @@
-import {
-	UnsubscribeParamsSchema,
-	deleteSubscription,
-} from "@/http/controllers/event-subscription/delete-subscription";
-import type { FastifyReply, FastifyRequest } from "fastify";
-import { beforeEach, describe, expect, it, vi } from "vitest";
-
-const body = {
-	event_id: "3ac41c9e-5b58-45cd-8ed7-f9a2b2a2ccf7",
-};
+import { app } from "@/app";
+import supertest from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 describe("http > controllers > event-subscription > delete-subscription", () => {
-	let mockReply: FastifyReply;
-	let mockRequest: FastifyRequest;
-
-	beforeEach(() => {
-		mockReply = {
-			status: vi.fn().mockReturnThis(),
-			send: vi.fn().mockReturnThis(),
-		} as unknown as FastifyReply;
-
-		mockRequest = {
-			body,
-			params: { event_id: "invalid-event-id" },
-		} as FastifyRequest;
+	beforeAll(async () => {
+		await app.ready();
 	});
 
-	it("should return 400 if route params are invalid", async () => {
-		const mockFormat = vi.fn().mockReturnValueOnce([
-			{
-				message: "Invalid event_id",
-				path: ["event_id"],
-				code: "invalid_type",
-			},
-		]);
-
-		const mockSafeParseParams = vi.fn().mockReturnValueOnce({
-			success: false,
-			error: {
-				format: mockFormat,
-				issues: [
-					{
-						message: "Invalid event_id",
-						path: ["event_id"],
-						code: "invalid_type",
-					},
-				],
-				message: "Invalid event_id",
-				isEmpty: false,
-			},
-		});
-
-		UnsubscribeParamsSchema.safeParse = mockSafeParseParams;
-
-		await deleteSubscription(mockRequest, mockReply);
-
-		expect(mockReply.status).toHaveBeenCalledWith(400);
-		expect(mockReply.send).toHaveBeenCalledWith({
-			error: "Invalid params",
-			issues: [
-				{
-					message: "Invalid event_id",
-					path: ["event_id"],
-					code: "invalid_type",
-				},
-			],
-		});
+	afterAll(async () => {
+		await app.close();
 	});
 
-	it("should return 400 if body params are invalid", async () => {
-		const mockFormatBody = vi.fn().mockReturnValueOnce([
-			{
-				message: "Invalid body data",
-				path: ["data"],
-				code: "invalid_type",
-			},
-		]);
-
-		const mockSafeParseParams = vi.fn().mockReturnValueOnce({
-			success: false,
-			error: {
-				format: mockFormatBody,
-				issues: [
-					{
-						message: "Invalid event_id",
-						path: ["event_id"],
-						code: "invalid_type",
-					},
-				],
-				message: "Invalid event_id",
-				isEmpty: false,
-			},
+	it.only("should delete subscription and return 204", async () => {
+		const createdEvent = await supertest(app.server).post("/events").send({
+			title: "Title XYZ",
+			image_url:
+				"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAAA6ElEQVR4nO3QQQ3AIADAQJhxpIOF9UVI7hQ0nXuswT/f7YCXmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFZgVmBWYFRzgswJGYSWUJwAAAABJRU5ErkJggg==",
+			description: "XXXX",
+			tag: "Tag X",
+			event_date: "2012-04-23T18:25:43.511Z",
+			address: "Fifth Avenue, 785",
+			max_participants: "100",
 		});
 
-		UnsubscribeParamsSchema.safeParse = mockSafeParseParams;
+		const { id } = createdEvent.body.event;
 
-		await deleteSubscription(mockRequest, mockReply);
+		const createdSubcription = await supertest(app.server)
+			.post(`/events/subscribe/${id}`)
+			.send({
+				birthdate: new Date(),
+				cellphone: "511111",
+				email: "john.doe@example.com",
+				entity: "ds",
+				name: "john doe",
+			});
 
-		expect(mockReply.status).toHaveBeenCalledWith(400);
-		expect(mockReply.send).toHaveBeenCalledWith({
-			error: "Invalid params",
-			issues: [
-				{
-					message: "Invalid body data",
-					path: ["data"],
-					code: "invalid_type",
-				},
-			],
-		});
+		const { event_id } = createdSubcription.body.subscription;
+		const response = await supertest(app.server)
+			.delete(`/events/subscribe/${event_id}`)
+			.send({
+				email: "john.doe@example.com",
+			});
+		expect(response.statusCode).toBe(204);
+	});
+
+	it("should return 400 if params are invalid", async () => {
+		const response = await supertest(app.server).delete("/event/subscribe/1");
+
+		expect(response.statusCode).toBe(400);
 	});
 });
